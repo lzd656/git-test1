93d08aefbd8607fafe1ca82b7e0a7b9d0c6cead1
goul <1193949178@qq.com>
2021-09-26 14:34:50 +0800

    second commit

--- src/main/java/guigu/git/gogo.java
+++ src/main/java/guigu/git/gogo.java
@@ -2,6 +2,7 @@ package guigu.git;
 
 public class gogo {
     public static void main(String[] args) {
-        System.out.println("666666666");
+        System.out.println("6666666");
+        System.out.println("6666666");
     }
 }
